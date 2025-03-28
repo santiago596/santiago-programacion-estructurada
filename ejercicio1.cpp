ab6301784a3cda575ba7d1215d2016015fa711ff
santiago596 <santiagomedina0404@gmail.com>
2025-03-28 08:03:56 -0500

    Update ejercicio1.cpp

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main()
 {
 
-    for(int i=50;i<=100;i+=2)
-    {//Notemos que escribir i+=2 es similar a escribir i = i + 2
+    for(int i=50;i<=100;i+=2){
         cout << i << endl;
     }
     system("PAUSE");
